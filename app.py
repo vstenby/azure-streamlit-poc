269604ef2842921449e18e929913057947887215
Viktor Stenby Johansson <viktor.stenby@vippsmobilepay.com>
2023-11-02 14:30:17 +0100

    Added app

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#Make the app wide.
+st.set_page_config(layout="wide")
+
+st.markdown("# FCP Streamlit App")
+st.write("This is a proof of concept Streamlit app.")
+
+#To begin with, let's add a text field that simply just counts the letters.
+s = st.text_input("Enter some text here:")
+st.write("Your string has", len(s), "characters.")
