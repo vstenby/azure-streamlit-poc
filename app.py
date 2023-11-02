0520e3230ba26b466e1694add6369244937d7723
Viktor Stenby Johansson <viktor.stenby@vippsmobilepay.com>
2023-11-02 14:51:13 +0100

    Updated app

--- app.py
+++ app.py
@@ -9,3 +9,5 @@ st.write("This is a proof of concept Streamlit app.")
 #To begin with, let's add a text field that simply just counts the letters.
 s = st.text_input("Enter some text here:")
 st.write("Your string has", len(s), "characters.")
+
+st.write('Hello from Python!')
